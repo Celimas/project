809bd7c2f5f05f3637af4530187156362de3b7f2
Tyler Marklyn <tmarklyn@cs.hmc.edu>
2014-11-30 22:46:30 -0800

    Working on semantics for deck

--- src/semantics.py
+++ src/semantics.py
@@ -1,25 +1,31 @@
 # A temporary file for my semantics with post-processing
+import Deck # Fix this to get the Deck.py file
 
 class deckSemantics(object):
+    deck = Deck()
+
     def deck(self, ast):
+        print "Finished processing deck:" + deck
         return ast
 
     def traits(self, ast):
-        return ast
-
-    def trait_names(self, ast):
+        print "Finished processing traits"
         return ast
 
     def trait_name(self, ast):
         return ast
 
     def trait_def(self, ast):
+        t = Trait(name, type) #TODO: Make this get values from the AST
+        print "  Adding" + t + "to traits"
+        deck.addTrait(t)
         return ast
 
     def typed_collection(self, ast):
         return ast
 
     def cards(self, ast):
+        print "Finished processing cards"
         return ast
 
     def card_rule(self, ast):
