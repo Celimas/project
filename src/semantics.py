a45dc22180fadcb7f5af9778dc39a4f656ab6ce3
Tyler Marklyn <tmarklyn@cs.hmc.edu>
2014-11-30 22:01:05 -0800

    Added a file for my semantics

---
+++ src/semantics.py
@@ -0,0 +1,44 @@
+# A temporary file for my semantics with post-processing
+
+class deckSemantics(object):
+    def deck(self, ast):
+        return ast
+
+    def traits(self, ast):
+        return ast
+
+    def trait_names(self, ast):
+        return ast
+
+    def trait_name(self, ast):
+        return ast
+
+    def trait_def(self, ast):
+        return ast
+
+    def typed_collection(self, ast):
+        return ast
+
+    def cards(self, ast):
+        return ast
+
+    def card_rule(self, ast):
+        return ast
+
+    def foreach_rule(self, ast):
+        return ast
+
+    def traits_rule(self, ast):
+        return ast
+
+    def combine_rule(self, ast):
+        return ast
+
+    def make_rule(self, ast):
+        return ast
+
+    def alphanumeric(self, ast):
+        return ast
+
+    def numeric(self, ast):
+        return ast
